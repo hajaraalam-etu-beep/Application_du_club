586899e98f6a6e0e31d0f412b7bc56501d7cf425
Hajar Aalam <hajar.aalam-etu@etu.univh2c.ma>
2026-05-31 18:12:33 +0100

    modification

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 function Footer() {
   return (
     <footer id="contact"  className="bg-dark text-white text-center p-4 mt-5">
-      <h5>Contact</h5>
       <p>Email :  <a href="mailto:aidevcommunityfsbm@gmail.com"> aidevcommunityfsbm@gmail.com</a></p>
       <p>Téléphone :<a href="tel:+212600000000"> +212629471191</a></p>
       <p>Instagram : <a href="https://www.instagram.com/aidev_communityfsbm?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">aidevcommunityfsbm</a></p>
