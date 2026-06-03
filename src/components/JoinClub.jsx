b048afd6713414b871c2a66846e40cda35cb0328
Hajar Aalam <hajar.aalam-etu@etu.univh2c.ma>
2026-06-03 23:03:43 +0100

    Description des modifications

--- src/components/JoinClub.jsx
+++ src/components/JoinClub.jsx
@@ -3,14 +3,19 @@ import React, { useState } from "react";
 function JoinClub() {
   const [showForm, setShowForm] = useState(false);
   const [message, setMessage] = useState("");
-
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [email, setEmail] = useState("");
   const envoyer = () => {
     setMessage("Votre demande a été envoyée avec succès !");
+
+    setNom("");
+    setPrenom("");
+    setEmail("");
   };
 
   return (
     <div id="join" className="container text-center mt-5">
-
       <h2>Rejoindre le club</h2>
 
       <button
@@ -22,42 +27,35 @@ function JoinClub() {
 
       {showForm && (
         <form className="mt-4">
-
           <input
             type="text"
             placeholder="Nom"
             className="form-control mb-3"
+            value={nom}
+            onChange={(e) => setNom(e.target.value)}
           />
-
           <input
             type="text"
             placeholder="Prénom"
             className="form-control mb-3"
+            value={prenom}
+            onChange={(e) => setPrenom(e.target.value)}
           />
-
           <input
             type="email"
             placeholder="Email"
             className="form-control mb-3"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
-          <button
-            type="button"
-            className="btn btn-success"
-            onClick={envoyer}
-          >
+          <button type="button" className="btn btn-success" onClick={envoyer}>
             Envoyer
           </button>
-
         </form>
       )}
 
-      {message && (
-        <p className="mt-3 text-success fw-bold">
-          {message}
-        </p>
-      )}
-
+      {message && <p className="mt-3 text-success fw-bold">{message}</p>}
     </div>
   );
 }
