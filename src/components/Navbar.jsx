645004c1a2e7c3ffd6e58de2e622d2ad1871c518
Hajar Aalam <hajar.aalam-etu@etu.univh2c.ma>
2026-06-03 23:09:02 +0100

    derniere modification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,72 @@
 import React from "react";
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg custom-navbar">
+    <nav className="navbar navbar-expand-lg custom navbar">
       <div className="container">
-
-        <img
-          src="/image/logo.jpg"
-          alt="logo"
-          width="50"
-          className="logo"
-        />
+        <img src="/image/logo.jpg" alt="logo" width="50" className="logo" />
 
         <span className="navbar-brand ms-2 fw-bold">
           Club Aidev_communityfsbm
         </span>
 
         <div className="navbar-nav ms-auto">
-          <a className="nav-link custom-link" href="#members">
+          <a
+            className="nav-link custom-link"
+            href="#members"
+            style={{
+              fontWeight: "bold",
+              fontSize: "18px",
+              color: "black",
+            }}
+          >
             Membres
           </a>
 
-          <a className="nav-link custom-link" href="#events">
+          <a
+            className="nav-link custom-link"
+            href="#events"
+            style={{
+              fontWeight: "bold",
+              fontSize: "18px",
+              color: "black",
+            }}
+          >
             Événements
           </a>
 
-          <a className="nav-link custom-link" href="#gallery">
+          <a
+            className="nav-link custom-link"
+            href="#gallery"
+            style={{
+              fontWeight: "bold",
+              fontSize: "18px",
+              color: "black",
+            }}
+          >
             Galerie
           </a>
 
-          <a className="nav-link custom-link" href="#join">
+          <a
+            className="nav-link custom-link"
+            href="#join"
+            style={{
+              fontWeight: "bold",
+              fontSize: "18px",
+              color: "black",
+            }}
+          >
             Rejoindre
           </a>
 
-          <a className="nav-link custom-link" href="#contact">
+          <a
+            className="nav-link custom-link"
+            href="#contact"
+            style={{
+              fontWeight: "bold",
+              fontSize: "18px",
+              color: "black",
+            }}
+          >
             Contact
           </a>
         </div>
@@ -40,4 +74,5 @@ function Navbar() {
     </nav>
   );
 }
+import { useState } from "react";
 export default Navbar;
